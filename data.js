8506c0d8d220c7f9d8de6ae550e0de33cb4cac96
ZamaNcongwana <qadrantmusic1@gmail.com>
2023-08-30 18:14:39 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+let songs = [ 
+    {
+        name: 'uThando alusekho',
+        path: 'musics/Mistoh x Sne  _ uThando alusekho_2.mp3',
+        artist: 'Mistoh',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'ISbani',
+        path: 'musics/Mistoh_ISbani_-_.mp3',
+        artist: 'Mistoh',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'Thethelela',
+        path: 'musics/Mistoh ft Mshengu x Da Enrew__Thethelea.mp3',
+        artist: 'Mistoh x Da Enrew x Mshengu',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'My Dali',
+        path: 'musics/Mistoh ft Msindlo x Last Born__My Dali.mp3',
+        artist: 'Mistoh x Msindlo',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'Letha yona',
+        path: 'musics/ngixoleleMISTOH.mp3',
+        artist: 'Mistoh',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'Mama',
+        path: 'musics/Mama.wav',
+        artist: 'Mistoh x Da Enrew x Msanezie',
+        cover: 'images/Ubani lo.png'
+    },
+    {
+        name: 'Whatever it is',
+        path: 'musics/Mistoh x Mshengu x Ayanda__Whatever its.mp3',
+        artist: 'Mistoh x Mshengu x Ayanda',
+        cover: 'images/Ubani lo.png'
+    },
+
+
+]
